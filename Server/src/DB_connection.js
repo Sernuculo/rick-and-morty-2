8ed1035c0a7f95905dfc4a3b5d22b32fa0f2dab7
Micaela <micaescu99@gmail.com>
2023-07-26 17:46:51 -0300

    26/07

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -4,7 +4,9 @@ const path = require('path');
 const envPath = path.join(__dirname, '.env');
 dotenv.config({ path: envPath });
 
-const { DB_USER, DB_PORT, DB_HOST } = process.env;
+const {
+    DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+ } = process.env;
 
 const { Sequelize } = require('sequelize');
 const { users, favorites } = require('./models/index');
@@ -18,7 +20,7 @@ const { users, favorites } = require('./models/index');
 // "postgres://postgres@localhost:3232/rickandmorty"
 // `postgres://${DB_USER}@${DB_HOST}:${DB_PORT}/rickandmorty`
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}@${DB_HOST}:${DB_PORT}/rickandmorty`,
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/rickandmorty`,
    { logging: false, native: false }
 );
 
